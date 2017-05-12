6c7d54500bfb21ff7fa864758638329f1d890d3e
Connor Lenio <cojamalo@gmail.com>
2017-05-12 11:06:33 -0400

    Updates

--- mult_regres.R
+++ mult_regres.R
@@ -1,15 +1,14 @@
 
 # Title: Multiple Linear Regression Tools
 # Date: 2017-04-28
+# Most Recent Update: 2017-05-11
 # Author: Connor Lenio
 # Dependencies: dplyr, DAAG, AICcmodavg
 # Enhances: leaps::regsubsets, stats::AIC, stats::step, DAAG::cv.lm, AICcmodavg::AICc
 # Description: Functions to evalute linear fit for the purposes of multiple regression and predictor selection
 
-# Warning: These functions were constructed to assist in learning about linear regression and evaluating linear
-# models with the mtcars data set and may or may not be helpful to actual use outside of simple regression using 
-# data like mtcars. Currently, they only work with fully numerical variables only. Categorical variables must be 
-# converted to numerical variables for the functions to work.
+# Why: These functions were constructed to assist in learning about linear regression and evaluating linear models
+# with the mtcars data set and may or may not be helpful to actual use outside of simple regression using data like mtcars
 
 ### Please note ###
 # Depending on your specific case, it will be better to use alternative methods of model selection such as:
@@ -24,6 +23,60 @@
 # best_step <- step(null, scope=list(lower=null, upper=full))
 ###
 
+## Load Dependencies
+library(DAAG)
+library(AICcmodavg)
+library(dplyr)
+
+
+## Function that takes a model and the data such as: 
+# model_eval(fit.BIC, new_mtcars)
+# Note: 10x10 K Fold CV is optional to save computation time
+# Set kfold = True to include 10x10 K Fold CV
+# Return: A single row with scores for each analysis metric 
+#                           model terms adj_R_2 BIC AICc LOOCV KFOLD
+#  mpg ~ am + recip_hp + log_wt     3   0.882   151  145  5.31  5.81
+model_eval <- function(model, data, LOO_CV = FALSE, kfold = FALSE) {
+    options(warn=-1)
+    LOOCV = NA
+    if (LOO_CV) {
+        # For kfold, m = k for LOOCV
+        one_run <- CVlm(data, model, m = nrow(data), printit = FALSE, plotit = FALSE)
+        # 5 x 2 Fold Mean Square
+        LOOCV <- attributes(one_run)$ms
+    }
+    MSE = NA
+    if (kfold) {
+        # 10x10 K-Fold Cross Validation
+        ms <- c()
+        for (i in 1:10) {
+            ms_run <- CVlm(data, model, m = 10, seed = i, printit = FALSE, plotit = FALSE)
+            ms_run <- attributes(ms_run)$ms 
+            ms <- c(ms, ms_run) 
+        }
+        MSE <- mean(ms)
+        MSE_hi <- round(1.96 * sqrt(MSE),digits=2)
+        MSE_lo <- round(-1.96 * sqrt(MSE),digits=2)
+    }
+    options(warn=0)
+    # Model code
+    model_name <- attributes(model$model)$terms
+    model_name <- Reduce(paste, deparse(model_name, width.cutoff = 500))
+    # Adjusted R^2
+    R_2 <- summary(model)$adj.r.squared
+    #Parsimony
+    predictors <- length(attributes(summary(model)$terms)$term.labels)
+    # AICc Analysis
+    AICc <- AICc(model) 
+    # BIC Analysis
+    BIC <- BIC(model)
+    # RMSE
+    fitted = predict(model, se.fit=TRUE)
+    RMSE <- sqrt(mean((model$model[1] - fitted$fit)^2))
+    # MSE <- mean(ms)
+    output <- data.frame(model = model_name, terms = predictors, adj_R_2 = R_2, BIC=BIC, AICc = AICc, RMSE = RMSE, LOOCV = LOOCV, KFOLD = MSE) 
+    return(output)
+}
 
 ## Helper function that takes input such as the following: 
 # find_fit(mpg, wt, mtcars, "Log", lm(mpg~log(wt), mtcars))
@@ -31,12 +84,11 @@
 #   type terms adj_R_2 BIC AICc LOOCV KFOLD
 #    Log     1   0.804 162  158  7.64    NA
 find_fit <- function(response, explanatory, data, type, model) {
-    library(dplyr)
     data$response <- eval(substitute(response), data)
     data$explanatory <- eval(substitute(explanatory), data)
     out <- tryCatch(model, error = function(e) e)
     if(any(class(out) == "error")) {
-        output <- data.frame(type = type, terms = NA, adj_R_2 = NA,BIC=NA,AICc = NA, LOOCV = NA, KFOLD = NA)
+        output <- data.frame(type = type, terms = NA, adj_R_2 = NA,BIC=NA,AICc = NA, RMSE = NA, LOOCV = NA, KFOLD = NA)
     }
     else {
         output <- cbind(type = type, select(model_eval(model, data),-model))
@@ -47,8 +99,8 @@ find_fit <- function(response, explanatory, data, type, model) {
 ## Function that takes input such as the following: 
 # compare_fit(mpg, disp, mtcars)
 # Returns: multiple row data frame for each transformation type ranked by LOOCV score 
-compare_fit <- function(response, explanatory, data) {
-    library(dplyr)
+# Options: Can select the target metric for model fit: BIC, AICc, or RMSE (default)
+compare_fit <- function(response, explanatory, data, target = "RMSE") {
     if (class(response) == "character") {
         response <- as.name(response)
         explanatory <- as.name(explanatory)
@@ -67,15 +119,15 @@ compare_fit <- function(response, explanatory, data) {
                     find_fit(response, explanatory, data, "Cube", lm(response~I(explanatory^3), data)),
                     find_fit(response, explanatory, data, "Square Root", lm(response~sqrt(explanatory), data)),
                     find_fit(response, explanatory, data, "Cubic Root", lm(response~I(explanatory^(1/3)), data)))
-    output <- output %>% arrange(LOOCV)
-    output <- output[complete.cases(output[,-7]),]
-    if(any(output$type %in% "Exponential") & round(filter(output, type == "Exponential")$LOOCV[1], digits = 4) == round(output$LOOCV[1], digits = 4)) {
+    output <- output %>% arrange(output[,target]) %>% select(-LOOCV:-KFOLD)
+    output <- output[complete.cases(output),]
+    if(any(output$type %in% "Exponential") & isTRUE(all.equal(filter(output, type == "Exponential")[[target]][1], output[[target]][1], tolerance = 0.0001))) {
         output <- rbind(filter(output, type == "Exponential"), filter(output, type != "Exponential"))
     }
-    if(any(output$type %in% "Log") & round(filter(output, type == "Log")$LOOCV[1], digits = 4) == round(output$LOOCV[1], digits = 4)) {
+    if(any(output$type %in% "Log") & isTRUE(all.equal(filter(output, type == "Log")[[target]][1], output[[target]][1], tolerance = 0.0001))) {
         output <- rbind(filter(output, type == "Log"), filter(output, type != "Log"))
     }
-    if(any(output$type %in% "Linear") & round(filter(output, type == "Linear")$LOOCV[1], digits = 4) == round(output$LOOCV[1], digits = 4)) {
+    if(any(output$type %in% "Linear") & isTRUE(all.equal(filter(output, type == "Linear")[[target]][1], output[[target]][1], tolerance = 0.0001))) {
         output <- rbind(filter(output, type == "Linear"), filter(output, type != "Linear"))
     }
     return(output)
@@ -83,19 +135,19 @@ compare_fit <- function(response, explanatory, data) {
 
 ## Function that takes input such as the following: 
 # find_best_trans(mpg, mtcars)
-# Returns: multiple-row data frame with the "best" linear transformation for each variable in the data 
-find_best_trans <- function(response, data) {
-    library(dplyr)
+# Returns: multiple-row data frame with the "best" linear transformation for each variable in the data
+# Options: Can select the target metric for model fit: BIC, AICc, or RMSE (default)
+find_best_trans <- function(response, data, target = "RMSE") {
     print("Processing, please wait.....")
     output <- data.frame()
     for (i in 1:ncol(data)) {
             response <- as.character(substitute(response))
             explan <- names(data)[i]
-            row <- compare_fit(response, explan, data)[1,]
+            row <- compare_fit(response, explan, data, target)[1,]
             row <- cbind(data.frame(variable = explan), row)
             output <- rbind(output, row)
     }
-    output <- output[-1,] %>% arrange(LOOCV, AICc) %>% select(-KFOLD)
+    output <- output[-1,] %>% arrange(RMSE, AICc)
     return(output)
    
 }
@@ -116,78 +168,36 @@ add_best_trans <- function(best_trans_df, data) {
                "Log2" = {new_col <- log2(raw_vector); data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0("log2_",var_name)},
                "Exponential" = {new_col <- exp(raw_vector); data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0("exp_",var_name)},
                "Exp10" = {new_col <- 10^(raw_vector); data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0("10^",var_name)},
-               "Exp2" = {new_col <- 2^raw_vector; data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0("2^",var_name)},
+               "Exp2" = {new_col <- 2^raw_vector; data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0("2_",var_name)},
                "Reciprical" = {new_col <- 1/(raw_vector); data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0("recip_",var_name)},
-               "Square" = {new_col <- raw_vector^2; data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0(var_name, "^2")},
-               "Cube" = {new_col <- raw_vector^3; data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0(var_name, "^3")},
+               "Square" = {new_col <- raw_vector^2; data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0(var_name, "_2")},
+               "Cube" = {new_col <- raw_vector^3; data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0(var_name, "_3")},
                "Square Root" = {new_col <- sqrt(raw_vector); data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0("sqrt_",var_name)},
-               "Cubic Root" = {new_col <- raw_vector^(1/3); data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0(var_name, "^1/3")}
+               "Cubic Root" = {new_col <- raw_vector^(1/3); data <- cbind(data, new_col); names(data)[length(names(data))] <- paste0(var_name, "_1/3")}
                ) 
     }
    
     return(data)
 }
 
-## Function that takes a model and the data such as: 
-# model_eval(fit.BIC, new_mtcars)
-# Note: 10x10 K Fold CV is optional to save computation time
-# Set kfold = True to include 10x10 K Fold CV
-# Return: A single row with scores for each analysis metric 
-#                           model terms adj_R_2 BIC AICc LOOCV KFOLD
-#  mpg ~ am + recip_hp + log_wt     3   0.882   151  145  5.31  5.81
-model_eval <- function(model, data, kfold = FALSE) {
-    options(warn=-1)
-    # For kfold, m = k for LOOCV
-    one_run <- CVlm(data, model, m = nrow(data), printit = FALSE, plotit = FALSE)
-    MSE = NA
-    if (kfold) {
-        # 10x10 K-Fold Cross Validation
-        ms <- c()
-        for (i in 1:10) {
-            ms_run <- CVlm(data, model, m = 10, seed = i, printit = FALSE, plotit = FALSE)
-            ms_run <- attributes(ms_run)$ms 
-            ms <- c(ms, ms_run) 
-        }
-        MSE <- mean(ms)
-        MSE_hi <- round(1.96 * sqrt(MSE),digits=2)
-        MSE_lo <- round(-1.96 * sqrt(MSE),digits=2)
-    }
-    options(warn=0)
-    # Model code
-    model_name <- as.character(model)[11]
-    # Adjusted R^2
-    R_2 <- summary(model)$adj.r.squared
-    #Parsimony
-    predictors <- length(attributes(summary(model)$terms)$term.labels)
-    # AICc Analysis
-    AICc <- AICc(model) 
-    # BIC Analysis
-    BIC <- BIC(model)
-    # 5 x 2 Fold Mean Square
-    LOOCV <- attributes(one_run)$ms
-    # MSE <- mean(ms)
-    output <- data.frame(model = model_name, terms = predictors, adj_R_2 = R_2, BIC=BIC, AICc = AICc,LOOCV = LOOCV, KFOLD = MSE) 
-    return(output)
-}
-
-
-
 
 
 # Note: Please install DAAG bug fix to use tree_lm with: install_github("gokceneraslan/DAAG")
 ## Controller Function that takes a base function as a string and the data such as: 
-# tree_lm("mpg ~ am", new_mtcars), or
-# target argument is which analysis to optimize for
-# tree_lm("mpg ~ am", new_mtcars, target = "AICc"), or
-# Top_n is the number of top options to further explore at each run of check_models
-# tree_lm("mpg ~ am", new_mtcars, top_n = 3), or
-# Note: 10x10 K Fold CV is optional to save computation time
-# Set kfold = True to include 10x10 K Fold CV
-# tree_lm("mpg ~ am", new_mtcars, kfold = TRUE)
-# Please use 1 instead of a period in your formula if you want to begin the search with no predictors such as: "mpg ~ 1"
+# tree_lm("mpg ~ am", new_mtcars)
 # Return: A data frame with the "best model" found for each number of predictors
+# Options: -target argument is which model fit metric to optimize for such as:
+# tree_lm("mpg ~ am", new_mtcars, target = "AICc"), where
+# Target options are: BIC, AICc, RMSE (default), LOOCV, KFOLD, or
+# -Top_n is the number of top options to further explore at each run of check_models
+# tree_lm("mpg ~ am", new_mtcars, top_n = 3), or
+# -10x10 K Fold CV and Leave One Out CV are optional to save computation time
+# Set kfold = True to include 10x10 K Fold CV
+# tree_lm("mpg ~ am", new_mtcars, kfold = TRUE, LOOCV = TRUE)
+# The function will automatically set these to TRUE if you select them as targets
+# -Please use 1 instead of a period in your formula if you want to begin the search with no predictors such as: "mpg ~ 1"
 # Warning: Optimization is the root of all evil i.e. know why you are optimizing and its costs!
-tree_lm <- function(base_function, data, target = c("LOOCV","BIC", "AICc", "KFOLD"), top_n = 3, kfold = FALSE) {
+tree_lm <- function(base_function, data, target = c("RMSE"), top_n = 3, LOO_CV = FALSE, kfold = FALSE) {
     options(warn=-1)
     library(dplyr)
     library(AICcmodavg)
@@ -198,9 +208,12 @@ tree_lm <- function(base_function, data, target = c("LOOCV","BIC", "AICc", "KFOL
     if (target == "KFOLD") {
         kfold = TRUE  
     }
-    output <- model_eval(null_mod, data, kfold=kfold)
+    if (target == "LOOCV") {
+        LOO_CV = TRUE  
+    }
+    output <- model_eval(null_mod, data, LOO_CV = LOO_CV,kfold=kfold)
     best <- output[[target]] 
-    output <- check_models(base_function, data, top_n, best, target, kfold)
+    output <- check_models(base_function, data, top_n, best, target, LOO_CV, kfold)
     output <- arrange(output, output[,target])
     # This checks for any rows that are duplicates of each other:
     duplicates <- duplicated(apply(apply( output, 1, sort), 2 , paste , collapse = ""))
@@ -213,13 +226,17 @@ tree_lm <- function(base_function, data, target = c("LOOCV","BIC", "AICc", "KFOL
 
 # Function run inside tree_lm that uses recursion to search a tree of possible predictor combinations to find the "best" options 
 # for a given model quality analysis
-check_models <- function(base_function, data, top_n, best, target, kfold = FALSE) {
+check_models <- function(base_function, data, top_n, best, target, LOO_CV = FALSE, kfold = FALSE) {
     if (target == "KFOLD") {
       kfold = TRUE  
     }
+    if (target == "LOOCV") {
+        LOO_CV = TRUE  
+    }
     null_mod <- lm(base_function, data)
+    
     #Construct the formulas from a given base and possible variables
-    output <- model_eval(null_mod, data, kfold = kfold)
+    output <- model_eval(null_mod, data, LOO_CV = LOO_CV, kfold = kfold)
     # extract response variable
     params <- trimws(strsplit(base_function, split = "~")[[1]])
     variables <- names(data)
@@ -250,11 +267,11 @@ check_models <- function(base_function, data, top_n, best, target, kfold = FALSE
     # Construct formulas vector   
     formulas <- apply(current, 1 , paste , collapse = " + ")    
     formulas <- paste0(params[1], " ~ ", formulas)
-   # Runs model eval for each of a vector of formula strings
-    for (formula in formulas)   { 
+    # Runs model eval for each of a vector of formula strings
+    for (formula in formulas)   {
         new_mod <- lm(formula, data)
         # Run model_eval on constructed model
-        new_row <- model_eval(new_mod, data, kfold=kfold)
+        new_row <- model_eval(new_mod, data, LOO_CV = LOO_CV, kfold=kfold)
         output <- rbind(output, new_row)
     }
     # # Check for duplicates
@@ -267,15 +284,16 @@ check_models <- function(base_function, data, top_n, best, target, kfold = FALSE
     form_length <- top_n - (top_n - nrow(output))
     if (form_length > top_n) {form_length = top_n}
     formulas <- as.character(output[1:form_length,1])
-    if (is.na(formulas)) {
+     if (is.na(formulas)) {
      return(output)   
-    }
+     }
+    
     if (base_function %in% formulas) {
            formulas <- formulas[-which(base_function == formulas)]
     } 
     for (formula in formulas) {
-        
-          output <- rbind(output, check_models(formula, data, top_n, best, target, kfold))  
+        print(formula)
+          output <- rbind(output, check_models(formula, data, top_n, best, target, LOO_CV,kfold))  
     }
     return(output)
 }
@@ -293,12 +311,3 @@ my_fn <- function(data, mapping, ...){
     p
 }
 
-## Function timing blocks
-#### Troubleshoot ####
-start <- Sys.time()
-#### Troubleshoot ####
-
-#### Troubleshoot ####     
-end <- Sys.time()
-print(end-start)
-#### Troubleshoot ####
